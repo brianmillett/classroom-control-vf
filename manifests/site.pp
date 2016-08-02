71df0d2e226b6652e499f4da9bc8ca7f7c71b23f
brianmillett <bmillett@gmail.com>
2016-08-02 14:57:24 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,16 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    content => "Remember, no matter where you go, there you are.\n",
+#  file { '/etc/motd':
+#    ensure => file,
+#    owner => 'root',
+#    group => 'root',
+#    mode => '0644',
+#    content => "Remember, no matter where you go, there you are.\n",
+#  }
+  
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr/bin:/usr/local/bin',
+    creates => '/etc/motd',
   }
 }
