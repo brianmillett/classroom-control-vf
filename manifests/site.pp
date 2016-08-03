2929c9dce578d732677cc04c2c865e91222a2d35
brianmillett <bmillett@gmail.com>
2016-08-03 12:57:50 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,29 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
+
+node 'brianmillett.puppetlabs.vm' {
+
+  include examples::fundamentals
+  include users
+  include skeleton
+  include memcached
+  
+#  file { '/etc/motd':
+#    ensure => file,
+#    owner => 'root',
+#    group => 'root',
+#    mode => '0644',
+#    content => "Remember, no matter where you go, there you are.\n",
+#  }
+  
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr/bin:/usr/local/bin',
+    creates => '/etc/motd',
+  }
+  
+  host { 'testing.puppetlabs.vm':
+    ensure => present,
+    ip => '127.0.0.1',
+  }
+}
