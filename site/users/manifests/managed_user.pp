8b8088e82b55d84f67b259be2e637c04d3a57672
brianmillett <bmillett@gmail.com>
2016-08-04 12:26:09 -0500

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,21 @@
+define users::managed_user (
+  $group = $title,
+) {
+  user { $title:
+    ensure => present,
+  }
+
+  file { "/home/${title}":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+  }
+
+  file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+    mode  => 'go-rwx',
+  }
+
+}
